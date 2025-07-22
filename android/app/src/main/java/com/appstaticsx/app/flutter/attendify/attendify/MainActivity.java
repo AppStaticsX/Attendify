5edc12a0bb389562a57ac6832098443506c953ac
AppStaticsX <uanushka2001@gmail.com>
2025-07-22 19:46:58 +0530

    improve local authentication

--- android/app/src/main/java/com/appstaticsx/app/flutter/attendify/attendify/MainActivity.java
+++ android/app/src/main/java/com/appstaticsx/app/flutter/attendify/attendify/MainActivity.java
@@ -1,6 +1,6 @@
 package com.appstaticsx.app.flutter.attendify.attendify;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
 }
